c25eaded412bfd9922542842d87d50e92109f19d
DreamMaoMao <2523610504@qq.com>
2023-11-20 08:51:31 +0800

    更正配置适配arch

--- statusbar/packages/vol.sh
+++ statusbar/packages/vol.sh
@@ -28,8 +28,8 @@ update() {
     is_blue=$( pactl list sinks | grep -E "Name:|名称：" | grep blue )
     sink=$(pactl info | grep 'Default Sink' | awk '{print $3}')
     if [ "$sink" = "" ]; then sink=$(pactl info | grep '默认音频入口' | awk -F'：' '{print $2}');fi
-    volunmuted=$(pactl list sinks | grep $sink -A 6 | sed -n '7p' | grep '静音：否')
-    vol_text=$(pactl list sinks | grep $sink -A 7 | sed -n '8p' | awk '{printf int($4)}')
+    volunmuted=$(pactl list sinks | grep $sink -A 6 | sed -n '7p' | grep -E 'Mute: no|静音：否')
+    vol_text=$(pactl list sinks | grep $sink -A 7 | sed -n '8p' | awk '{printf int($5)}')
     if [ "$LANG" != "zh_CN.UTF-8" ]; then
         volunmuted=$(pactl list sinks | grep $sink -A 6 | sed -n '7p' | grep 'Mute: no')
         vol_text=$(pactl list sinks | grep $sink -A 7 | sed -n '8p' | awk '{printf int($5)}')
