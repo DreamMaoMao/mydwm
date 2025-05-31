da9e92f2917236c83cb1c07b8700ec7f9ac5cba8
DreamMaoMao <2523610504@qq.com>
2025-05-31 23:18:37 +0800

    u

--- autostart.sh
+++ autostart.sh
@@ -13,9 +13,9 @@ settings() {
 	# pkill -f xdg-desktop-portal-gnome
 	# pkill clash
 	# picom --experimental-backends --config $DWM/scripts/config/picom.conf & # 开启picom
-	picom --config $DWM/scripts/config/picom.conf &
+	picom --config $DWM/scripts/config/picom.conf 2> /dev/null &
 	# feh --randomize --bg-fill $DWM/wallpaper/caoyuan.jpg & # 壁纸
-	feh --randomize --bg-fill $DWM/wallpaper/car.jpeg & # 壁纸
+	feh --randomize --bg-fill $DWM/wallpaper/car.jpeg 2> /dev/null & # 壁纸
 	# echo "Xft.dpi: 140" | xrdb -merge                      #dpi缩放
 	xrdb -merge ~/.Xresources
   # cp ~/.config/zellij/configx11.kdl ~/.config/zellij/config.kdl
@@ -23,23 +23,23 @@ settings() {
 	# 开启输入法
 	fcitx5 -r -d 2> /dev/null &
 	# bash /opt/apps/com.baidu.fcitx-baidupinyin/files/bin/bd-qimpanel.watchdog.sh &	#百度拼音输入法
-	dunst -config ~/.config/dunst/dwm_dunstrc & # 开启通知server
+	dunst -config ~/.config/dunst/dwm_dunstrc 2> /dev/null & # 开启通知server
 	# systemctl --user unmask xdg-desktop-portal-gnome
 	# systemctl --user mask xdg-desktop-portal-hyprland
-	/usr/libexec/xdg-desktop-portal &
-	redshift &
-	parcellite &
-	blueman-applet &
-	nm-applet &
-	/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+	# /usr/libexec/xdg-desktop-portal &
+	redshift 2> /dev/null &
+	parcellite 2> /dev/null &
+	blueman-applet 2> /dev/null &
+	nm-applet 2> /dev/null &
+	/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 2> /dev/null &
 	numlockx on
-	$_thisdir/statusbar/statusbar.sh cron & # 开启状态栏定时更新
+	$_thisdir/statusbar/statusbar.sh cron 2> /dev/null & # 开启状态栏定时更新
 	[ -e /dev/sda4 ] && udisksctl mount -t ext4 -b /dev/sda4
 	# python3 ~/tool/sign.py &
 	cp ~/.config/eww/System-Menu/eww.yuck.x11  ~/.config/eww/System-Menu/eww.yuck
-	eww daemon &
+	eww daemon 2> /dev/null &
 
-  flameshot &
+  flameshot 2> /dev/null &
 	# plank &
 }
 
